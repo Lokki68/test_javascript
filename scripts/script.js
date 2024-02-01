fbc66790e80d3bb2f9c8414b9e5a940f3170800e
LIBERT Brice <81553708+Lokki68@users.noreply.github.com>
2024-02-01 13:40:00 +0100

    add style

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/**
+ * Cette fonction affiche dans la console le score de l'utilisateur
+ * @param {number} score: le score de l'utilisateur
+ * @param {number} nbMotProposes: le nombre de mots proposés à l'utilisateur
+ */
+function afficherResultat(score, nbMotProposes) {
+    console.log(`Votre score est de ${score} sur ${nbMotProposes}`)
+}
+
+/**
+ * Cette fonction demande à l'utilisateur de choisir entre "mots" et "phrases" et retourne le choix de l'utilisateur
+ * @return {string} : le choix de l'utilsateur
+ */
+function name(param) {
+
+}
